0f88501bdbff8e0613d140815abf8e2e8a4ee405
yubi00 <ubrajkhadka@gmail.com>
2021-01-07 01:36:59 +1100

    startup msg changed

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ app.delete("/todos/:id", async (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on Port ${port}`);
 });
