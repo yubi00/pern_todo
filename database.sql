0e8d264ab9e4ef7793d2451ed5d8283eec5c87bc
yubi00 <ubrajkhadka@gmail.com>
2021-01-07 02:52:09 +1100

    heroku deploy

--- database.sql
+++ database.sql
@@ -4,3 +4,7 @@ CREATE TABLE todo (
     id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+/*open postgresql inside the heroku */
+//heroku pg:psql -a yubi-pern-todo 
+//heroku addons:create heroku-postgresql:hobby-dev -a yubi-pern-todo
+/* this creates postgresql addons */
